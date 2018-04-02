ad0908f7e52863b5af062b40d5fc13081d0d0b0c
ibrahimhss <kekoreko3@gmail.com>
2018-04-02 03:36:47 +0200

    ibrahim second commet

--- app/src/main/java/com/abunadeem/newapp/home.java
+++ app/src/main/java/com/abunadeem/newapp/home.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-//todooooooooooooooooooo
+//TODO first commet
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
